96df3cf61286333e65b5f899905810b65b9e1db4
James Jelenko <jamesjelenko@gmail.com>
2024-09-12 10:16:07 -0700

    remove old directories following cleanup

--- gambler-backend/gambler/config/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite', // or 'postgres' or 'mysql'
-  storage: 'database.sqlite', // SQLite file path
-  logging: false, // Set to true for debugging
-});
-
-module.exports = sequelize;
